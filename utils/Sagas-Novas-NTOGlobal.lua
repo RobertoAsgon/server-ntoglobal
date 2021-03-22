79fffab82451d8c6500f0796a05d793c8097b3a3
Roberto Asgon <robertoasgon@gmail.com>
2021-03-21 22:50:56 -0300

    chore: adiciona sagas e refatora spells.xml

---
+++ utils/Sagas-Novas-NTOGlobal.lua
@@ -0,0 +1,102 @@
+-- 8 Sagas Free + 4 Saga Missão + 1 Efeito ao transformar
+
+-- Naruto
+[1] = { 1508, 1507, 951, 952, 92, 954, 955, 743, Anbu= 876, Akat= 877, Otsu= 1412, Kage= 1434, Efeito = 600  }
+-- Sasuke 
+[2] = { 2, 5, 6, 7, 93, 94, 165, 1792, Anbu= 878, Akat= 879, Otsu= 1415, Kage= 1434, Efeito = 443  }
+-- Sakura 
+[3] = { 175, 173, 172, 169, 170, 194, 978, 1796, Anbu= 880, Akat= 881, Otsu= 1397, Kage= 1287, Efeito = 5  } 
+-- Rock Lee
+[4] = { 83, 78, 80, 190, 497, 191, 1535, 1536, Anbu= 888, Akat= 889, Otsu= 1411, Kage= 1434, Efeito = 617  }
+-- Neji
+[5] = { 101, 104, 105, 108, 488, 489, 962, 963, Anbu= 890, Akat= 891, Otsu= 1413, Kage= 1434, Efeito = 513  }
+-- Tenten
+[6] = { 1831, 1830, 1835, 1833, 1832, 1836, 1834, 1876, Anbu= 1807, Akat= 893, Otsu= 1402, Kage= 1294, Efeito = 620  }
+-- Gaara
+[7] = { 52, 53, 67, 72, 58, 61, 1945, 1946, Anbu= 904, Akat= 905, Otsu= 1408, Kage= 1434, Efeito = 142  }
+-- Kiba
+[8] = { 50, 69, 70, 196, 198, 199, 1947, 1948, Anbu= 894, Akat= 895, Otsu= 1410, Kage= 1269, Efeito = 23  }
+-- Shikamaru 
+[9] = { 1828, 1827, 426, 1821, 1819, 1820, 1824, 1795, Anbu= 936, Akat= 937, Otsu= 1417, Kage= 1434, Efeito = 196  }
+-- Chouji 
+[10] = { 433, 440, 441, 1439, 1437, 1438, 1435, 1891, Anbu= 938, Akat= 939, Otsu= 1407, Kage= 1434, Efeito = 593  }
+-- Hashirama 
+[11] = { 750, 751, 753, 754, 1465, 1468, 1470, 1561, Anbu= 918, Akat= 919, Otsu= 1379, Kage= 1434, Efeito = 386  }
+-- Tobirama 
+[12] = { 788, 789, 792, 795, 1522, 1524, 1525, 1842, Anbu= 916, Akat= 917, Otsu= 1403, Kage= 1434, Efeito = 397  }
+-- Hiruzen 
+[13] = { 1190, 1191, 1192, 1193, 1195, 1197, 1200, 1841, Anbu= 1238, Akat= 1239, Otsu= 1398, Kage= 1434, Efeito = 591  }
+-- Minato 
+[14] = { 560, 562, 563, 564, 565, 709, 998, 999, Anbu= 886, Akat= 887, Otsu= 1392, Kage= 1434, Efeito = 589  }
+-- Tsunade 
+[15] = { 586, 587, 588, 592, 594, 1622, 1624, 1627, Anbu= 914, Akat= 915, Otsu= 1404, Kage= 1295, Efeito = 622  }
+-- Kakashi 
+[16] = { 9, 10, 11, 13, 14, 1541, 1878, 1879, Anbu= 934, Akat= 935, Otsu= 1409, Kage= 1271, Efeito = 607  }
+-- Madara 
+[17] = { 476, 477, 481, 1460, 1463, 1504, 1205, 1609, Anbu= 942, Akat= 943, Otsu= 1960, Kage= 1434, Efeito = 592  }
+-- Hidan 
+[18] = { 528, 529, 530, 531, 539, 540, 1782, 1789, Anbu= 922, Akat= 923, Otsu= 1382, Kage= 1434, Efeito = 0  }
+-- Obito 
+[19] = { 1566, 1569, 1571, 1572, 1573, 1575, 1576, 1577, Anbu= 1802, Akat= 1812, Otsu= 1394, Kage= 1283, Efeito = 608  }
+-- Killer Bee 
+[20] = { 732, 733, 735, 736, 739, 740, 741, 742, Anbu= 884, Akat= 885, Otsu= 1380, Kage= 1263, Efeito = 607  }
+-- Kisame 
+[21] = { 1969, 1965, 1962, 1961, 1967, 1968, 1966, 1946, Anbu= 1963, Akat= 927, Otsu= 1390, Kage= 1434, Efeito = 622  }
+-- Konan 
+[22] = { 1614, 1615, 1616, 846, 847, 1619, 1620, 1794, Anbu= 931, Akat= 932, Otsu= 1391, Kage= 1434, Efeito = 620  }
+-- Temari 
+[23] = { 1959, 1958, 1957, 1956, 1952, 1950, 1953, 1701, Anbu= 908, Akat= 909, Otsu= 1401, Kage= 1298, Efeito = 597  }
+-- Nagato 
+[24] = { 853, 854, 855, 856, 1602, 1603, 968, 969, Anbu= 932, Akat= 933, Otsu= 1393, Kage= 1434, Efeito = 608  }
+-- Itachi 
+[25] = { 1552, 1549, 1545, 1546, 1548, 1551, 1804, 1805, Anbu= 928, Akat= 929, Otsu= 1385, Kage= 1434, Efeito = 592  }
+-- Shisui 
+[26] = { 1101, 1103, 1106, 1584, 1113, 1115, 1586, 1587, Anbu= 1118, Akat= 1119, Otsu= 1405, Kage= 1434, Efeito = 594  }
+-- Yamato 
+[27] = { 463, 1926, 1920, 1921, 1917, 1919, 1925, 1922, Anbu= 1923, Akat= 901, Otsu= 1406, Kage= 1434, Efeito = 386  }
+-- Suigetsu 
+[28] = { 1943, 1940, 1928, 1930, 1938, 1935, 1936, 1933, Anbu= 1934, Akat= 1728, Otsu= 1400, Kage= 1434, Efeito = 398  }
+-- Deidara 
+[29] = { 1869, 1865, 1864, 1870, 1868, 1866, 1867, 1872, Anbu= 944, Akat= 945, Otsu= 1381, Kage= 1434, Efeito = 594  }
+-- Orochimaru 
+[30] = { 1510, 1511, 1512, 1513, 1515, 1516, 1005, 1874, Anbu= 1793, Akat= 1518, Otsu= 1395, Kage= 1284, Efeito = 608  }
+-- Onoki
+[31] = { 1172, 1174, 1176, 1177, 1179, 1180, 1182, 1185, Anbu= 1187, Akat= 1186, Otsu= 1378, Kage= 1434, Efeito = 623  }
+-- Jiraiya 
+[32] = { 815, 817, 820, 824, 825, 827, 974, 975, Anbu= 940, Akat= 941, Otsu= 1386, Kage= 1434, Efeito = 594  }
+-- Shino 
+[33] = { 416, 421, 417, 439, 958, 959, 957, 1890, Anbu= 896, Akat= 1893, Otsu= 1399, Kage= 1434, Efeito = 69  }
+-- Sai 
+[34] = { 1894, 1895, 1896, 1815, 1897, 1816, 1817, 1818, Anbu= 1806, Akat= 1813, Otsu= 1414, Kage= 1286, Efeito = 243  }
+-- Kankuro
+[35] = { 619, 620, 622, 623, 1779, 1775, 1776, 1780, Anbu= 906, Akat= 907, Otsu= 1389, Kage= 1434, Efeito = 596  }
+-- Guy
+[36] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Hinata
+[37] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Ino
+[38] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Kabuto
+[39] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Kakuzo
+[40] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Raikage
+[41] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Sasori
+[42] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Zabuza
+[43] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Asuma
+[44] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Kimimaru
+[45] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Haku
+[46] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Menma
+[47] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Konohamaru
+[48] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Mei
+[49] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
+-- Zetsu
+[50] = { X, X, X, X, X, X, X, X, Anbu= X, Akat= X, Otsu= X, Kage= X, Efeito = X  }
